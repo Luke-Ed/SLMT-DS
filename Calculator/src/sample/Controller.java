ea1401b6c2a5163c817ca4aacefb413f89a193dc
Luke-Ed <luke_ed@outlook.com>
2019-10-14 15:56:29 -0400

    Made all of the simple button handling one line. Trying to think how I want to handle powers.

--- Calculator/src/sample/Controller.java
+++ Calculator/src/sample/Controller.java
@@ -7,26 +7,12 @@ import javafx.scene.control.TextArea;
 public class Controller {
   @FXML protected TextArea mainWriteArea;
 
-  @FXML protected void handleInCPress(ActionEvent act){
-    mainWriteArea.clear();
-  }
+  @FXML protected void handleInCPress(ActionEvent act){ mainWriteArea.clear(); }
   @FXML protected void handleSqRtPress(ActionEvent act){mainWriteArea.appendText("^(1/2)");}
-  @FXML protected void handleIn7Press(ActionEvent act){
-    mainWriteArea.appendText("7");
-  }
-  @FXML protected void handleIn8Press(ActionEvent act){
-    mainWriteArea.appendText("8");
-  }
-  @FXML protected void handleIn9Press(ActionEvent act){
-    mainWriteArea.appendText("9");
-  }
-  @FXML protected void handleInDPress(ActionEvent act){
-    mainWriteArea.appendText(" / ");
-  }
-  @FXML protected void handleInMPress(ActionEvent act){
-    mainWriteArea.appendText(" * ");
-  }
-  @FXML protected void handleInSqPress(ActionEvent act){
-    mainWriteArea.appendText(" ^2 ");
-  }
+  @FXML protected void handleIn7Press(ActionEvent act){ mainWriteArea.appendText("7"); }
+  @FXML protected void handleIn8Press(ActionEvent act){ mainWriteArea.appendText("8"); }
+  @FXML protected void handleIn9Press(ActionEvent act){ mainWriteArea.appendText("9"); }
+  @FXML protected void handleInDPress(ActionEvent act){ mainWriteArea.appendText(" / "); }
+  @FXML protected void handleInMPress(ActionEvent act){ mainWriteArea.appendText(" * "); }
+  @FXML protected void handleInSqPress(ActionEvent act){ mainWriteArea.appendText(" ^2 "); }
 }
