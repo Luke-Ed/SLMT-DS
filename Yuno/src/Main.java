a11ea936b668309b49698bde7420215f306bc5f8
Luke-Ed <luke_ed@outlook.com>
2019-11-01 10:16:47 -0400

    Started working on some of the FXML for Yuno.
    Made some of the basic documents necessary for the switch. I'm going to
    have to look at a few things, and possibly get this working with
    multiple XML documents.

--- Yuno/src/Main.java
+++ Yuno/src/Main.java
@@ -1,9 +1,24 @@
 
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Luke's a dumb bitch.");
-	}
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception{
+    Parent root = FXMLLoader.load(getClass().getResource("yuno.fxml"));
+      primaryStage.setTitle("Yuno");
+      primaryStage.setScene(new Scene(root, 300, 275));
+      primaryStage.show();
+    }
+
+
+  public static void main(String[] args) {
+        launch(args);
+    }
 }
