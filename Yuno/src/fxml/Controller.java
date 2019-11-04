bceae5061ecd775e20a8371d508d2390f7297b42
Luke-Ed <luke_ed@outlook.com>
2019-11-04 16:30:12 -0500

    Worked on Yuno
    Got Yuno to the same stage as Shelby's code

--- Yuno/src/fxml/Controller.java
+++ Yuno/src/fxml/Controller.java
@@ -1,5 +1,19 @@
 package fxml;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 
+public class Controller {
+  @FXML protected GridPane GridPane_Welcome;
+  @FXML protected GridPane GridPane_Setup;
+  @FXML protected GridPane GridPane_Game;
+
+  @FXML protected void Continue_Button(){
+    GridPane_Welcome.setVisible(false);
+    GridPane_Setup.setVisible(true);
+  }
+  @FXML protected void Continue_To_Game_Button(){
+    GridPane_Setup.setVisible(false);
+    GridPane_Game.setVisible(true);
+  }
 }
