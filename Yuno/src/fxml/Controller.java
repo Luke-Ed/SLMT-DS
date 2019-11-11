95806a13e6e3c1895d350aaf695b40521c35bc4f
Luke Edmondson <Luke_ed@outlook.com>
2019-11-11 10:25:51 -0500

    Added the "assets" package - going to work with getting a few cards in to begin with.
    This should be interesting, I'm aiming to spice up the title screen some, I might also work on making this dark theme.

--- Yuno/src/fxml/Controller.java
+++ Yuno/src/fxml/Controller.java
@@ -1,13 +1,21 @@
 package fxml;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class Controller {
   @FXML protected GridPane GridPane_Welcome;
   @FXML protected GridPane GridPane_Setup;
   @FXML protected GridPane GridPane_Game;
+  @FXML protected ImageView background_image;
 
+  @FXML protected void initialize(){
+    Image bkg_img = new Image("file:assets/yuno-card-background.png", 300, 0, true,false, true);
+    //Image bkg_img = new Image("https://www.google.com/logos/doodles/2019/veterans-day-2019-4689153982726144-l.png", true);
+    background_image.setImage(bkg_img);
+  }
   @FXML protected void Continue_Button(){
     GridPane_Welcome.setVisible(false);
     GridPane_Setup.setVisible(true);
