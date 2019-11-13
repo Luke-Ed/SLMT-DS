26d986bd7ce032516cf9e64adf3097925ce0579a
Luke-Ed <luke_ed@outlook.com>
2019-11-13 13:18:39 -0500

    Left comments on how to work with imageviews.

--- Yuno/src/fxml/Controller.java
+++ Yuno/src/fxml/Controller.java
@@ -10,12 +10,16 @@ public class Controller {
   @FXML protected GridPane GridPane_Setup;
   @FXML protected GridPane GridPane_Game;
   @FXML protected ImageView background_image;
-
+  //
+  //  Just because I couldn't find it documented well online, I thought I would include
+  //  a few options for how to add an image to an image view using an initialize in the controller
+  //  to repeat; this must go in the initialize method, or so I've discovered so far, this can possibly be changed on later methods
+  //  to react to a button press, etc.
+  //    Image bkg_img = new Image("https://www.google.com/logos/doodles/2019/veterans-day-2019-4689153982726144-l.png", true);
+  //    Image bkg_img = new Image("fxml/assets/yuno-card-background.png",300, 0, true, true, true);
+  //    background_image.setImage(bkg_img);
+  //
   @FXML protected void initialize(){
-    //Image bkg_img = new Image("/assets/yuno-card-background.png", 300, 0, true,false, true);
-    //Image bkg_img = new Image("https://www.google.com/logos/doodles/2019/veterans-day-2019-4689153982726144-l.png", true);
-    //Image bkg_img = new Image("https://github.com/Luke-Ed/DS-BS/blob/master/Yuno/src/fxml/assets/yuno-card-background.png");
-    //Image bkg_img = new Image("file:yuno-card-background.png");
     Image bkg_img = new Image("fxml/assets/yuno-card-background.png",300, 0, true, true, true);
     background_image.setImage(bkg_img);
   }
