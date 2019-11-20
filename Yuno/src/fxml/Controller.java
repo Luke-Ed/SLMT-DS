4df1fb386f807737ceb41448ad70c4ca0c2166e8
Luke Edmondson <Luke_ed@outlook.com>
2019-11-20 10:35:05 -0500

    Began work on setting up players, read from toggle-buttons.

--- Yuno/src/fxml/Controller.java
+++ Yuno/src/fxml/Controller.java
@@ -1,6 +1,8 @@
 package fxml;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -10,6 +12,8 @@ public class Controller {
   @FXML protected GridPane GridPane_Setup;
   @FXML protected GridPane GridPane_Game;
   @FXML protected ImageView background_image;
+  @FXML protected ToggleGroup PlayerButtons;
+  private int numPlayers; // For usage when generating players and hands, because this code hasn't been written yet, we have a warning.
   //
   //  Just because I couldn't find it documented well online, I thought I would include
   //  a few options for how to add an image to an image view using an initialize in the controller
@@ -29,6 +33,16 @@ public class Controller {
   }
   @FXML protected void Continue_To_Game_Button(){
     GridPane_Setup.setVisible(false);
+    String Selected =  ((RadioButton)PlayerButtons.getSelectedToggle()).getText();
+    if (Selected.equals("2 Players")) {
+      numPlayers = 2;
+    }
+    else if (Selected.equals("3 Players")) {
+      numPlayers = 3;
+    }
+    else {
+      numPlayers = 4;
+    }
     GridPane_Game.setVisible(true);
   }
 }
