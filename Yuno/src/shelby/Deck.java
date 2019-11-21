f27ae065e9592917091e752b00e864738bfff0ff
Shelby Parrish <sa.parrish@hotmail.com>
2019-11-21 18:35:13 -0500

    Lots (card changes, deck works mostly, only for full not discarded)

---
+++ Yuno/src/shelby/Deck.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Deck {
+	String[] colors = {"Purple","Yellow","Blue","Green"};
+	ArrayList<Card> orderedDeck = new ArrayList<Card>();
+	Stack<Card> deck = new Stack<Card>();
+	
+	public Deck() {
+		for(int s=0; s<2; s++) {
+			for (int i=0; i<4; i++) {
+				for (int j=1; j<11; j++) {
+					deck.add(new Card(colors[i], j));
+				}
+			}
+		}
+		for (int i=0; i<deck.size(); i++) {
+			orderedDeck.add(deck.get(i));
+		}
+		Collections.shuffle(orderedDeck);
+		deck.clear();
+		for (int i=0; i<orderedDeck.size(); i++) {
+			deck.add(orderedDeck.get(i));
+		}
+	}
+	
+	public Card draw() {
+		return deck.pop();
+	}
+	
+	public int size() {
+		return deck.size();
+	}
+}
---
+++ Yuno/src/shelby/Player.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+	String name;
+	Deck deck;
+	List<Card> hand = new ArrayList<Card>();
+
+	public Player(String name, Deck deck) {
+		this.name = name;
+		this.deck = deck;
+		for (int i=0; i<7; i++) {this.drawCard();}
+	}
+	
+	public String getHand() {
+		String cards = "";
+		for (int i=0; i<hand.size(); i++) {
+			cards += (hand.get(i).toString());
+		}
+		return cards;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void drawCard() {
+		hand.add(deck.draw());
+	}
+}
