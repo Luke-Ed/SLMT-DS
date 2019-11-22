572c9fb96b0154a7609648d64dd09eadc999ce42
Luke-Ed <luke_ed@outlook.com>
2019-11-22 13:39:10 -0500

    Added some comments to my test code so hopefully someone else won't get a headache from trying to read it.

--- Yuno/src/fxml/Luke_Test.java
+++ Yuno/src/fxml/Luke_Test.java
@@ -20,25 +20,33 @@ public class Luke_Test extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Card c = new Card("1", "purple");
+    // Create a card that will be defined, ie having a matching file.
+
     ObservableList<Card> player1cards;
     player1cards = FXCollections.observableArrayList();
     player1cards.add(c);
-    // I think this should work, however I don't have a great way to test it without properly implementing an ImageView of some kind.
+    // Create an observable list, which is necessary to display an ArrayList on a ListView, from what I've seen online.
+
     GridPane gridPane = new GridPane();
     ImageView imageView = new ImageView();
     imageView.setImage(c.getCardImage());
+    //Create an ImageView, which is necessary in order to display an image itself, and a GridPane to put everything in.
 
     ListView<Card> listView = new ListView<>();
     listView.setOrientation(Orientation.HORIZONTAL);
     listView.setItems(player1cards);
+    // The code here creates a ListView, set's the cards to be the items in the ListView. The ListView's orientation is also changed to be horizontal.
     //listView.setCellFactory(cardListView -> new CustomListCell());
+
     gridPane.add(new Label("Test"), 0, 0);
     gridPane.add(imageView, 0, 1);
     gridPane.add(listView, 0, 2);
+    //Some of the other elements to be displayed on our GUI
 
     Scene scene = new Scene(gridPane, 1000, 600);
     primaryStage.setScene(scene);
     primaryStage.setTitle("Luke Test");
     primaryStage.show();
+    // Required code to display the Scene, and GUI for the application.
   }
 }
