596e75ad49a0ddfb641dfd82a170d7d67421ce85
Luke Edmondson <Luke_ed@outlook.com>
2019-11-22 12:59:40 -0500

    Wrote test code.

--- Yuno/src/fxml/Luke_Test.java
+++ Yuno/src/fxml/Luke_Test.java
@@ -1,8 +1,25 @@
 package fxml;
 
-public class Luke_Test {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Luke_Test extends Application {
   public static void main(String[] args){
+
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
     Card c = new Card("1", "purple");
     // I think this should work, however I don't have a great way to test it without properly implementing an ImageView of some kind.
+    GridPane gridPane = new GridPane();
+    ImageView imageView = new ImageView();
+    imageView.setImage(c.getCardImage());
+    Scene scene = new Scene(gridPane, 600, 1000);
+    primaryStage.setScene(scene);
   }
 }
